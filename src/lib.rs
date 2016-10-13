e62a17a324c0a72398d8b3e16dee581679c7bc4e
Mathew Velasquez <mathewvq@gmail.com>
2016-10-13 02:36:09 -0400

    Corrected typo

--- src/lib.rs
+++ src/lib.rs
@@ -97,7 +97,7 @@ pub const LUA_OPBXOR: c_int = 9;
 pub const LUA_OPSHL: c_int = 10;
 pub const LUA_OPSHR: c_int = 11;
 pub const LUA_OPUNM: c_int = 12;
-pub const LUA_OBNOT: c_int = 13;
+pub const LUA_OPBNOT: c_int = 13;
 
 pub const LUA_OPEQ: c_int = 0;
 pub const LUA_OPLT: c_int = 1;
